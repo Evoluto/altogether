dc1534b01539e1021c33eb7e5857729f700b48a7
IAF Consulting, LLC <mark@iafconsult.com>
2021-03-29 15:13:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
+if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
     exit;
 }
 include_once("index.html"); ?>
