6bc39e0886ab007c575f3f953cccec984ec2abb6
IAF Consulting, LLC <mark@iafconsult.com>
2021-03-29 15:14:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+if(!isset($_SERVER["HTTPS"]))
 {
     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
     exit;
